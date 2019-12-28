7c9a5a57e7a9e0b27b052ac08fe96cc6e1d0c292
sjc <280373133@qq.com>
2019-12-28 16:35:56 +0800

    Change play video api

--- src/views/showvideo/playVideo.vue
+++ src/views/showvideo/playVideo.vue
@@ -29,27 +29,27 @@ export default {
   beforeMount() {
   },
   created() {
-    alert(this.vid)
-     if (this.vid == 1) {
-      this.videoAvatar =
-        "https://metube-backend.oss-cn-beijing.aliyuncs.com/WX20191213-195644.png";
-      this.videoUrl =
-        "https://metube-backend.oss-cn-beijing.aliyuncs.com/1576236687649500.mp4";
-    }
-    // getVideo(1)
-    //   .then(response => {
-    //     // console.log('哈哈')
-    //     const { data } = response;
-    //     const { avatar } = data;
-    //     const { url } = data;
-    //     this.videoAvatar = avatar;
-    //     // console.log(this.videoAvatar)
-    //     this.videoUrl = url;
-    //     // console.log(this.videoUrl)
-    //   })
-    //   .catch(error => {
-    //     alert(error);
-    //   });
+    // alert(this.vid)
+    //  if (this.vid == 1) {
+    //   this.videoAvatar =
+    //     "https://metube-backend.oss-cn-beijing.aliyuncs.com/WX20191213-195644.png";
+    //   this.videoUrl =
+    //     "https://metube-backend.oss-cn-beijing.aliyuncs.com/1576236687649500.mp4";
+    // }
+    getVideo(this.vid)
+      .then(response => {
+        // console.log('哈哈')
+        const { data } = response;
+        const { avatar } = data;
+        const { url } = data;
+        this.videoAvatar = avatar;
+        // console.log(this.videoAvatar)
+        this.videoUrl = url;
+        // console.log(this.videoUrl)
+      })
+      .catch(error => {
+        alert(error);
+      });
   }
 };
 </script>
