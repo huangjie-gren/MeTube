56ae431b8b9ecbaf924e042cb54706868ef94004
wangsicheng24 <247450210@qq.com>
2019-12-28 17:29:38 +0800

    merge

--- src/api/friendManagement.js
+++ src/api/friendManagement.js
@@ -1,27 +1,27 @@
 import request from '@/utils/request'
-
+/* eslint-disable */
 const preUrl = 'http://49.233.170.51:8080'
 
-  export function showFollowings(id) {
-    return request({
-      url: preUrl + '/showFollowings',
-      method: 'get',
-      params: { id }
-    })
-  }
+export function showFollowings(id) {
+  return request({
+    url: preUrl + '/showFollowings',
+    method: 'get',
+    params: { id }
+  })
+}
 
-  export function showFollowers(id) {
-    return request({
-      url: preUrl + '/showFollowers',
-      method: 'get',
-      params: { id }
-    })
-  }
+export function showFollowers(id) {
+  return request({
+    url: preUrl + '/showFollowers',
+    method: 'get',
+    params: { id }
+  })
+}
 
-  export function unfollow(uid1, uid2) {
-    return request({
-      url: preUrl + '/unfollow',
-      method: 'get',
-      params: { uid1, uid2 }
-    })
-  }
+export function unfollow(uid1, uid2) {
+  return request({
+    url: preUrl + '/unfollow',
+    method: 'get',
+    params: { uid1, uid2 }
+  })
+}
