b75cea94dba184ce7180a7989557b506588f7977
Crown798 <1391972018@qq.com>
2020-01-11 19:18:53 +0800

    .

--- src/utils/const.js
+++ src/utils/const.js
@@ -4,8 +4,8 @@ export const sjcURL = 'http://114.115.178.1:8081'
 export const wscURL = 'http://49.233.170.51:8080'
 // export const wscURL = 'http://localhost:8080'
 
-// export const crURL = 'http://114.115.149.142:8083'
-export const crURL = 'http://localhost:8083'
+export const crURL = 'http://114.115.149.142:8083'
+// export const crURL = 'http://localhost:8083'
 
 export const zjlURL = 'http://123.56.96.92:3001'
 // export const zjlURL = 'http://127.0.0.1:3000'
